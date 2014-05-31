e4170133026d26204a0d2d1339788dc314fef12c
Hassan Abdel-Rahman <hassan.abdelrahman@mhelabs.com>
2014-05-31 17:07:03 -0400

    added show hide to code samples

--- app/controllers/code-snippet.js
+++ app/controllers/code-snippet.js
@@ -1,5 +1,19 @@
 export default Ember.Controller.extend({
-  needs: ['code-snippets'],
+  needs: ['code-snippets', 'topic'],
   codeSnippets: Ember.computed.alias('controllers.code-snippets.codeSnippets'),
-  codeSnippet: Ember.computed.alias('content')
+  codeSnippet: Ember.computed.alias('content'),
+
+  toggleDetailLabel: function(){
+    if (this.get('showInstructionDetail')) {
+      return 'Hide Instructions';
+    } else {
+      return 'Show Instructions';
+    }
+  }.property('showInstructionDetail'),
+
+  actions: {
+    toggleDetail: function(){
+      this.set('showInstructionDetail', !!!this.get('showInstructionDetail'));
+    }
+  }
 });
