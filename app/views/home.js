4dbb99fd586cfdd83f23ccfc5aeb993f440a016e
Hassan Abdel-Rahman <hassan.abdelrahman@mhelabs.com>
2014-06-01 11:27:49 -0400

    Fixes #21 added scroll right on the intro

--- app/views/home.js
+++ app/views/home.js
@@ -1,5 +1,5 @@
-export
-default Ember.View.extend({
+export default Ember.View.extend({
+  classNames: ['home'],
 
   init: function() {
     this._super();
@@ -19,18 +19,32 @@ default Ember.View.extend({
   },
 
   didInsertElement: function() {
+    var _this = this;
+    var $window = $(window);
     var $titleContainer = this.$().find('.title-container');
     var topPadding = parseInt($titleContainer.css('padding-top').replace('px', ''), 10);
+    var rightPadding = parseInt($titleContainer.css('padding-right').replace('px', ''), 10);
+    var leftPadding = parseInt($titleContainer.css('padding-left').replace('px', ''), 10);
     var bottomPadding = parseInt($titleContainer.css('padding-bottom').replace('px', ''), 10);
-    var windowHeight = $(window).height() - (topPadding + bottomPadding) - 30;
+    var windowHeight = $window.height() - (topPadding + bottomPadding);
+    var windowWidth = $window.width() - (rightPadding + leftPadding);
     var titleHeight = 300;
 
     this.$().find('.title-container, .intro-container').css({
-      'min-height': windowHeight
+      'min-height': windowHeight,
+      'width': windowWidth - 30
     });
     this.$().find('.title').css({
-      'margin-top': (windowHeight / 2) - (titleHeight / 2) - 30
+      'margin-top': (windowHeight / 2) - (titleHeight / 2)
+    });
+    this.$().css({
+      width: (2 * windowWidth) +  (2 * leftPadding) + rightPadding - 10
     });
 
+    $titleContainer.unbind('click').click(function(){
+      $('body').animate({
+        scrollLeft: _this.$().find('.intro-container').offset().left
+      });
+    });
   }
 });
