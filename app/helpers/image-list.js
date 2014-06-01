2319f78b6ce317cca202c4882bc7a969c5be32d3
Hassan Abdel-Rahman <hassan.abdelrahman@mhelabs.com>
2014-05-31 20:25:05 -0400

    fixed bug around handling contained items in helper

--- app/helpers/image-list.js
+++ app/helpers/image-list.js
@@ -1,18 +1,22 @@
-export default Ember.Handlebars.makeBoundHelper(function(itemForRoute, item){
+export default Ember.Handlebars.makeBoundHelper(function(item, itemForRoute) {
   if (!item.get('imageUrl')) return;
-  var containedSnippets = itemForRoute.get('containedSnippets');
-  var itemIsContained = containedSnippets.get('length') && containedSnippets.indexOf(item) > -1;
+  var containedSnippets;
+  var itemIsContained;
   var imageUrls = item.get('imageUrl').split('|');
   var imageStyle = (item.get('imageStyle') || '').split('|');
   var imageHtml = '';
   var i;
   var style;
+  if (Ember.typeOf(itemForRoute) === 'instance') {
+    containedSnippets = itemForRoute.get('containedSnippets');
+    itemIsContained = containedSnippets.get('length') && containedSnippets.indexOf(item) > -1;
+  }
   if (imageStyle.length > imageUrls.length) {
     imageHtml += '<div class="image-container ' + (itemIsContained ? 'contained' : '') + '" style="' + imageStyle[imageUrls.length] + '">';
   } else {
     imageHtml += '<div class="image-container ' + (itemIsContained ? 'contained' : '') + '">';
   }
-  for (i = 0; i < imageUrls.length; i++){
+  for (i = 0; i < imageUrls.length; i++) {
     style = null;
     if (imageStyle.length) {
       style = imageStyle[i];
