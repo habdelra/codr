1c4bc56f3f35b00176891c1a8a4f90b6bde5aa80
Hassan Abdel-Rahman <hassan.abdelrahman@mhelabs.com>
2014-06-03 00:13:47 -0400

    default the code snippet decrption to be expanded

--- app/controllers/code-snippet.js
+++ app/controllers/code-snippet.js
@@ -1,5 +1,6 @@
 export default Ember.Controller.extend({
   needs: ['code-snippets', 'topic'],
+  showInstructionDetail: true,
   codeSnippets: Ember.computed.alias('controllers.code-snippets.codeSnippets'),
   codeSnippet: Ember.computed.alias('content'),
 
